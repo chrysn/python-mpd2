d87ab76c1aa1fe757b3a6d082f7518afac295b0e
chrysn <chrysn@fsfe.org>
2014-04-19 19:16:54 +0200

    Revert "adapted some examples"
    due to the new compatibility layer, the old examples can be used as-is.
    
    This reverts commit d98e3c6ad661af7870a634a9f5fa53d6e38e4c21.

--- examples/idle.py
+++ examples/idle.py
@@ -3,25 +3,26 @@
 #cf. official documentation for further details:
 #
 #http://www.musicpd.org/doc/protocol/ch02.html#id525963
+#Example
+from select import select
 
-import asyncio
-import mpd
+client.send_idle()
+# do this periodically, e.g. in event loop
+canRead = select([client], [], [], 0)[0]
+if canRead:
+    changes = client.fetch_idle()
+    print(changes) # handle changes
+    client.send_idle() # continue idling
 
-loop = asyncio.get_event_loop()
-run = loop.run_until_complete
+#You can also poll the socket FD (returned by client.fileno(), which is called by default by select, poll, etc.) using other tools too.
 
-client = mpd.MPDClient()
-run(client.connect('localhost', 6600))
+#For example, with glib/gobject:
 
-for i in range(3):
-    print(run(client.idle()))
+def callback(source, condition):
+   changes = client.fetch_idle()
+   print(changes)
+   return False  # removes the IO watcher
 
-# You can also run this inside a coroutine
-
-@asyncio.coroutine
-def main():
-    for i in range(3):
-        changes = yield from client.idle()
-        print(changes)
-
-loop.run_until_complete(main())
+client.send_idle()
+gobject.io_add_watch(client, gobject.IO_IN, callback)
+gobject.MainLoop().run()
--- examples/randomqueue.py
+++ examples/randomqueue.py
@@ -1,14 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # IMPORTS
-import asyncio
 from mpd import (MPDClient, CommandError)
 from random import choice
 from socket import error as SocketError
 from sys import exit
 
-run = asyncio.get_event_loop().run_until_complete
 
 ## SETTINGS
 ##
@@ -21,17 +19,17 @@ PASSWORD = False
 client = MPDClient()
 
 try:
-    run(client.connect(host=HOST, port=PORT))
+    client.connect(host=HOST, port=PORT)
 except SocketError:
     exit(1)
 
 if PASSWORD:
     try:
-        run(client.password(PASSWORD))
+        client.password(PASSWORD)
     except CommandError:
         exit(1)
 
-run(client.add(choice(list(run(client.list('file'))))))
+client.add(choice(client.list('file')))
 client.disconnect()
 
 # VIM MODLINE
